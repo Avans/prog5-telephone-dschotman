2de1a1010a8ee4c84c05b7f1cb72d20e986c7423
Daniël Schotman <dfschotm@avans.nl>
2016-09-19 15:02:42 +0200

    Add person model

---
+++ Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephonebook.Models
+{
+    class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int TelephoneNumber { get; set; }
+
+        public String Fullname
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public Person(String firstName, String lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+    }
+}
